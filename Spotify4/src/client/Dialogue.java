f89a50a34c4fabebfbc4c63dfdc9fe121a404338
draguarMatth <50259605+draguarMatth@users.noreply.github.com>
2020-05-31 19:56:51 +0200

    Tout dans spotify4
    Fin logs
    +
    probablement quasi tout Annotation java doc

---
+++ Spotify4/src/client/Dialogue.java
@@ -0,0 +1,209 @@
+package client;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+
+import LogsConstructor.LoggerWithFileHandler;
+
+
+/**
+ * This class initialize 
+* @author Quentin Beeckmans - Mathieu Roux
+* @version 1.0
+* @since 2020-05-30
+*/
+
+public class Dialogue implements Runnable {
+
+	private Socket clientSocketOnServer;
+    private LoggerWithFileHandler logsServer;
+	
+	private ObjectOutputStream send;
+	private ObjectInputStream reader;
+	
+	public MyList mList;
+	
+	public ArrayList<String> serverList = new ArrayList<String>() ;
+	
+	private int portListening; //pour savoir le port pour d�livrer la musique
+	private Scanner scan = new Scanner(System.in);
+	
+	/**
+	 * This constructor implements an interface to ask some questions at the client
+	 * @param clientSocket
+	 * @param port
+	 * @param mList
+	 */
+	public Dialogue (Socket clientSocket, int port, MyList mList, LoggerWithFileHandler logsClient) {
+		this.clientSocketOnServer = clientSocket;
+		this.portListening=port;
+		this.mList=mList;
+		this.logsServer = logsClient;
+
+   	 	logsServer.addHandler(Dialogue.class.getName(), Level.INFO, "Nouveau client: IP: "  + clientSocketOnServer.getInetAddress().toString(), "port d'écoute " + String.valueOf(port));
+	}
+
+	public String getPort() {
+		return Integer.toString(portListening);
+		
+	}
+	
+	public String getIp() {
+		return clientSocketOnServer.getInetAddress().toString();
+		
+	}
+	
+	/**
+	 * This method allows to choice to send music repertory and listen a music fron an other client
+	 */
+	@Override
+	public void run() {
+		int choix;
+    	
+		do {
+			System.out.println("Que voulez-vous faire ?");
+			System.out.println("1 : Ajouter des musiques");
+			System.out.println("2 : Ecouter des musiques");
+			System.out.println("3 : Se d�connecter");
+			
+			choix = scan.nextInt();
+			
+			ClientSocket.ClientLogger.info("Choix fait " + choix);
+	         
+			switch(choix) {
+	         
+	         case 1: 
+	        	 System.out.println("J'ajoute des musiques au serveur");
+	        	 mList.sendFileList(this);
+	        	 logsServer.addHandler(Dialogue.class.getName(), Level.INFO, "Client choosed : add music on server", "");
+
+	        	 readList();
+	        	 break;
+	        	 
+	         case 2: 
+	        	 System.out.println("J'affiche les musiques du serveur: ");
+	        	 
+	        	 displayMusics(serverList);
+	        	 logsServer.addHandler(Dialogue.class.getName(), Level.INFO, "Client choosed : display available music", "");
+	        	 break;
+	        	 
+	         case 3: 
+		         System.out.println("Je sors");
+		         try {
+					Thread.sleep(5000);
+					send.close();
+			         reader.close();
+			         clientSocketOnServer.close();
+
+			         logsServer.addHandler(Dialogue.class.getName(), Level.WARNING, "Client disconnected","");
+				} catch (InterruptedException | IOException e) {
+					logsServer.addHandler(Dialogue.class.getName(), Level.SEVERE, "Client connexion interrupted with server", e.toString());
+					e.printStackTrace();
+				}
+		         
+	        	 break;
+	         }
+		}while(choix!=3);
+	}
+
+		     
+	        
+	/**
+	 * This method allows to get an arrayList from socket inputstream
+	 */
+	synchronized public void readList () {
+		
+		try {
+			reader = new ObjectInputStream(clientSocketOnServer.getInputStream());
+			ArrayList<String> arrayList = (ArrayList<String>) reader.readObject();
+			serverList = arrayList;
+		} catch (ClassNotFoundException | IOException e) {
+			logsServer.addHandler(ClientSocket.class.getName(), Level.SEVERE, "Problem with list of music reception", e.toString());
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	/**
+	 * This method allows to send an arrayList from socket outpustream
+	 */
+	public void sendObject(ArrayList<String> list) {
+		try {
+			send = new ObjectOutputStream(clientSocketOnServer.getOutputStream());
+			send.writeObject(list);
+			send.flush();
+		} catch (IOException e) {
+			logsServer.addHandler(Dialogue.class.getName(), Level.FINE,  "sendObject crash",  e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * This method allows to display an arrayList to make a choice of music
+	 */
+	public void displayMusics(ArrayList<String> serverList2){
+		   int cpt = 0;
+		   
+		   try {
+			   
+		   System.out.println("Choisissez une musique: ");
+		   for(String lineList: serverList2) {
+			   System.out.println(cpt + " : "  + lineList);
+			   cpt++;
+		   }
+		   int m=scan.nextInt();
+		   
+			logsServer.addHandler(Dialogue.class.getName(), Level.INFO,"Music choice: " + String.valueOf(m), "");
+
+		   String choice = serverList2.get(m);
+		   String [] address = choice.split(";");
+		   String str = address[1];
+		   String strNew = str.replace("/", "");
+		   
+
+		   newServerConnection(strNew, address[0], address[2]); 
+			}
+			catch (IllegalThreadStateException | ArrayIndexOutOfBoundsException e) {
+			
+				logsServer.addHandler(Dialogue.class.getName(), Level.SEVERE, "Display list crashed", e.toString());
+			}
+	}
+
+	/**
+	 * This method allows to do a newer connection Socket with the client who have the music choice and get stream audio after
+	 */
+	private void newServerConnection(String Ipaddress, String port, String musiquePath) {
+		try {
+			Socket exchangeSocket = new Socket(Ipaddress, Integer.parseInt(port));
+			System.out.println("Je suis connect� au client pour �couter");
+			
+			ClientSocket.ClientLogger.info("Client client connect� au client serveur ");
+			logsServer.addHandler(Dialogue.class.getName(), Level.INFO, "Client connected","");
+			
+			PrintWriter writerPath = new PrintWriter(exchangeSocket.getOutputStream());
+			writerPath.println(musiquePath);
+			writerPath.flush();
+			
+			InputStream is = new BufferedInputStream(exchangeSocket.getInputStream());
+			
+			SimpleAudioPlayer player = new SimpleAudioPlayer(is, logsServer);
+			player.play();
+			logsServer.addHandler(Dialogue.class.getName(), Level.INFO, "Début du streaming", musiquePath);
+			Thread.sleep(player.clip.getMicrosecondLength());
+		} catch (Exception e) {
+			logsServer.addHandler(Dialogue.class.getName(), Level.SEVERE, "New Server Connexion crash", e.getMessage());
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Spotify4/src/server/ClientS.java
@@ -0,0 +1,172 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.logging.Level;
+
+import LogsConstructor.LoggerWithFileHandler;
+
+/**
+ * This Runnable Class implements ClientS
+ * Create client on server
+ * Share server list with client
+ * Receive client list and add it into server list
+ * @author Quentin Beeckmans - Mathieu Roux
+ * @version 1.0
+ * @since 2020-05-30
+ */
+public class ClientS implements Runnable {
+	
+	private Socket clientSocket;
+	private int clientId;
+    private LoggerWithFileHandler logsServer;
+
+	private ArrayList<String> clientList = new ArrayList<String>() ;
+	
+	
+	private ObjectOutputStream send;
+	private ObjectInputStream reader;
+	
+	/**
+	 * Class contructor
+	 * @param clientId
+	 * @param clientSocket
+	 * @param logsServer
+	 * @author Quentin Beeckmans - Mathieu Roux
+	 * @version 1.0
+	 * @since 2020-05-30
+	 */
+	public ClientS(int clientId, Socket clientSocket, LoggerWithFileHandler logsServer) {
+		this.clientSocket=clientSocket;
+		this.clientId=clientId;
+	 	this.logsServer = logsServer;
+
+	}
+
+	@Override
+	public void run() {
+		try {
+			System.out.println("Client n� " + clientId + " IP" + clientSocket.getInetAddress());
+
+			send = new ObjectOutputStream(clientSocket.getOutputStream());
+
+			readList();
+       	 	logsServer.addHandler(ClientS.class.getName(), Level.INFO, "Communication with client : OK","Lists exchanged");
+
+		} catch (Exception e) {
+       	 	logsServer.addHandler(ClientS.class.getName(), Level.SEVERE, "Communication with client : KO",e.toString());
+			e.printStackTrace();
+		}
+
+	}
+	
+	/**
+	 * Synchronized public void method readList
+	 * Receive client list
+	 * @author Quentin Beeckmans - Mathieu Roux
+	 * @version 1.0
+	 * @since 2020-05-30
+	 */
+	synchronized public void readList () {
+		
+		try {
+			reader = new ObjectInputStream(clientSocket.getInputStream());
+			ArrayList<String> arrayList = (ArrayList<String>) reader.readObject();
+			clientList = arrayList;
+		} catch (ClassNotFoundException | IOException e) {
+       	 	logsServer.addHandler(ClientS.class.getName(), Level.SEVERE, "Impossible to read list",e.toString());
+			e.printStackTrace();
+		}
+		for(String item:clientList) {
+			System.out.println(item);
+		}
+		addToServer(clientId, clientList);
+	}
+		   
+	/**
+	 * Synchronized public void method addToServer
+	 * Add client list into server list
+	 * @param index in HasTable
+	 * @param ArrayList<String> list 
+	 * @author Quentin Beeckmans - Mathieu Roux
+	 * @version 1.0
+	 * @since 2020-05-30
+	 */
+	 synchronized public void addToServer(int index,ArrayList<String> list) {
+		System.out.println("AVANT PUT " + Server.serverList.keySet());
+		Server.serverList.put(index, list);
+		System.out.println("APRES PUT " + Server.serverList.keySet());
+		System.out.println("Mon index client pass� " + index);
+
+		logsServer.addHandler(ClientS.class.getName(), Level.INFO, "Music list added on server music list","");
+		   
+		shareGlobalList(index);
+	}
+
+	 /**
+	  * Private void shareGlobalList method of server
+	  * Search client list on server list for client applicant
+	  * @param index in HasTable
+	  * @author Quentin Beeckmans - Mathieu Roux
+	  * @version 1.0
+	  * @since 2020-05-30
+	  */
+	 private void shareGlobalList(int index) {
+		Set<Integer> clients = Server.serverList.keySet(); //on r�cup�re les key de chaque champs du Hashmap
+		ArrayList<String> lisToSend;
+		
+		
+		for(Integer key: clients){
+		System.out.println(key + " - " + index);
+			if(key!=index) {
+				System.out.println("J'ai trouvé un client");
+				lisToSend = Server.serverList.get(key);
+				sendObject(lisToSend);
+				System.out.println("Ca se passe ici");
+				for(String item:lisToSend) {
+					System.out.println(item);
+				}
+			}
+		}
+
+		logsServer.addHandler(ClientS.class.getName(), Level.INFO, "Music list shared with client","");
+	}
+	
+	 /**
+	  * Private void method shareGlobalList
+	  * Share client list on server list with client applicant
+	  * @param ArrayList<String> list 
+	  * @author Quentin Beeckmans - Mathieu Roux
+	  * @version 1.0
+	  * @since 2020-05-30
+	  */
+	 private void sendObject(ArrayList<String> list) {
+		try {
+			send.writeObject(list);
+			send.flush();
+		} catch (IOException e) {
+	   	 	logsServer.addHandler(ClientS.class.getName(), Level.SEVERE, "Music list sharing crashed", e.toString());
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+
+
